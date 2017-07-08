669a16e75d2609a40d392eac0fcca1a908798672
Meagan Blais <meaganblais@gmail.com>
2017-07-08 16:45:25 -0400

    fixing error

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ActiveRecord::Base
   validates :product_id, presence: true
   validates :user_id, presence: true
   validates :description, presence: true
-  validates :ratings, presence: true
+  validates :rating, presence: true
 end
