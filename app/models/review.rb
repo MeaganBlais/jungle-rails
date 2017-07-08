73713d85a16f6e5d41cc229a672342cb8d333139
Meagan Blais <meaganblais@gmail.com>
2017-07-08 14:44:17 -0400

    adding validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :ratings, presence: true
 end
