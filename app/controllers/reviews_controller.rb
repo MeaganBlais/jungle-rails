bc0ce1f35cbc0e3562fb592f11ebc9db849f8017
Meagan Blais <meaganblais@gmail.com>
2017-07-09 17:36:40 -0400

    fix routing error, limiting access to post a review to logged in users & user can delete own comment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,10 @@
 class ReviewsController < ApplicationController
 
+  before_action :authorize, only: [:create, :destroy]
+
   def create
-    @review = Review.new(review_params)
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
     @review.user = current_user
     @review.product_id = params[:product_id]
 
@@ -12,8 +15,17 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
 
+    if current_user.id == @review.user_id
+      @review.delete
+    redirect_to "/products/#{params[:product_id]}"
+    end
+  end
 
+  private
 
   def review_params
     params.require(:review).permit(
