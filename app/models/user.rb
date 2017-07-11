4402a21e74d0f75c099ac6fecf5b46204b3b9a75
Meagan Blais <meaganblais@gmail.com>
2017-07-10 20:12:00 -0400

    add new authentication class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { minimum: 7 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,28 +6,50 @@ RSpec.describe User, type: :model do
       expect(User.new).to be_a User
     end
     it 'should have valid input' do
-      @user = User.new ({ first_name: 'Mark', last_name: 'Smith', email: 'mark@gmail.com', password: 'easy', password_confirmation: 'easy' })
+      @user = User.new ({ first_name: 'Mark', last_name: 'Smith', email: 'mark@gmail.com', password: 'easy123', password_confirmation: 'easy123' })
       expect(@user.save).to be true
     end
     it 'has a password' do
-      @user = User.new({ first_name: 'Fred',last_name: 'Smith', email: 'fred@gmail.com', password: nil, password_confirmation: 'password' })
+      @user = User.new({ first_name: 'Fred',last_name: 'Smith', email: 'fred@gmail.com', password: nil, password_confirmation: 'easy123' })
       @user.save
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
     it 'has a password confirmation' do
-      @user = User.new({ first_name: 'Fred',last_name: 'Smith', email: 'fred@gmail.com', password: 'easy', password_confirmation: nil })
+      @user = User.new({ first_name: 'Fred',last_name: 'Smith', email: 'fred@gmail.com', password: 'easy123', password_confirmation: nil })
       @user.save
       expect(@user.errors.full_messages).to include "Password confirmation can't be blank"
     end
-
     it 'passwords must match' do
-      @user = User.new({ first_name: 'Fred',last_name: 'Smith', email: 'fred@gmail.com', password: 'easy', password_confirmation: 'easier' })
+      @user = User.new({ first_name: 'Fred',last_name: 'Smith', email: 'fred@gmail.com', password: 'easy123', password_confirmation: 'easier' })
       @user.save
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
+    it 'email must be unique' do
+      @user1 = User.create({ first_name: 'Freddie',last_name: 'Smith', email: 'freddy@gmail.com', password: 'easy123', password_confirmation: 'easy123' })
+      @user2 = User.new({ first_name: 'Freddy',last_name: 'Smith', email: 'freddy@gmail.com', password: 'easy123', password_confirmation: 'easy123' })
+      @user2.save
+      expect(@user2.errors.full_messages).to include "Email has already been taken"
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
 
 
+  it "return user when passed correct params" do
+    @user = User.create({ first_name: 'Freddy',last_name: 'Smith', email: 'freddy@gmail.com', password: 'easy123', password_confirmation: 'easy123' })
+    expect(User.authenticate_with_credentials(@user.email, @user.password)).to eql @user
+  end
 
+  it "return nil when passed incorrect email" do
+    @user = User.create({ first_name: 'Freddy',last_name: 'Smith', email: 'freddy@gmail.com', password: 'easy123', password_confirmation: 'easy123' })
+    expect(User.authenticate_with_credentials("incorrect_email", @user.password)).to eql nil
+  end
+
+  it "return nil when passed incorrect password" do
+    @user = User.create({ first_name: 'Freddy',last_name: 'Smith', email: 'freddy@gmail.com', password: 'easy123', password_confirmation: 'easy123' })
+    expect(User.authenticate_with_credentials(@user.email, "incorrect_password")).to eql nil
+end
 
 
 
@@ -36,4 +58,13 @@ RSpec.describe User, type: :model do
 
 
   end
+
+
+
+
+
+
+
+
+
 end
